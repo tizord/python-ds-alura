ca7c3cc99e257fde2d756dd178ff0dffc78efa7b
Tizord-Linux <thiagowpsousa@gmail.com>
2024-02-18 14:21:19 -0300

    pandas i-o

---
+++ pandas-i-o/.venv/lib/python3.10/site-packages/XLML/__init__.py
@@ -0,0 +1,1 @@
+from .xlml import XLML
---
+++ pandas-i-o/.venv/lib/python3.10/site-packages/XLML/xlml.py
@@ -0,0 +1,15 @@
+class XLML:
+    def __init__(self):
+        pass
+    
+    def process(self):
+        pass
+    
+    def convert(self, path: str):
+        html = "<html>"
+
+        html += "</html>"
+        return html
+
+if __name__ == "__main__":
+    pass
